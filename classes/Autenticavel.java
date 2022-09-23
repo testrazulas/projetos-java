521998cc8d6466d07656860e48c4388f76ee7637
Tiago Severo <tiago.estrazulas@sc.senai.br>
2022-09-23 08:35:21 -0300

    Transformando Autenticavel para Interface e extendendo a herança da Classe Diretor e Gerente para a classe Funcionario.

--- classes/Autenticavel.java
+++ classes/Autenticavel.java
@@ -1,34 +1,13 @@
 package classes;
 
-public abstract class Autenticavel extends Funcionario {
+public abstract interface Autenticavel {
 
-    private int senha;
 
-    private int login;
+    public void setSenha(int senha);
 
-    public void setSenha(int senha) {
-        this.senha = senha;
-    }
+    public void setLogin(int login);
 
-    public void setLogin(int login){
-        this.login = login;
-    }
+    public boolean autentica(int senha);
 
-    public boolean autentica(int senha){
-        if(this.senha == senha){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean autentica(int senha, int login){
-        if(this.senha == senha){
-            if(this.login == login){
-            }
-            return true;
-        } else {
-            return false;
-        }
-    }
+    public boolean autentica(int senha, int login);
 }
